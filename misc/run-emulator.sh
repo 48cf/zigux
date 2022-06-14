328a5bb4df572e5ce5b52d8633270469592d9b45
czapek1337 <czapek1337@gmail.com>
2022-06-14 19:54:33 +0200

    build: Fix passing additional args to QEMU

--- misc/run-emulator.sh
+++ misc/run-emulator.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
 qemu_args="-cdrom $1 -debugcon stdio -smp 1 -m 1G -M q35,accel=kvm:whpx:tcg -cpu qemu64,+smap,+smep -no-reboot -no-shutdown -s"
-qemu_args+=${@:2}
 
-qemu-system-x86_64 ${qemu_args}
+qemu-system-x86_64 ${qemu_args} ${@:2}
