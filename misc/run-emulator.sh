d5c8357eacac39cb002daab319fdb742c03192c4
czapek1337 <czapek1337@gmail.com>
2022-06-14 18:19:55 +0200

    build: Use HW acceleration is possible

--- misc/run-emulator.sh
+++ misc/run-emulator.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-image_path=$1
+qemu_args="-cdrom $1 -debugcon stdio -smp 1 -m 1G -M q35,accel=kvm:whpx:tcg -cpu qemu64,+smap,+smep -no-reboot -no-shutdown -s"
+qemu_args+=${@:2}
 
-qemu-system-x86_64 -cdrom $1 -debugcon stdio -smp 1 -m 1G -M q35 \
-    -cpu qemu64,+smap,+smep -no-reboot -no-shutdown -s ${@:2}
+qemu-system-x86_64 ${qemu_args}
