81f621d571b629383560b02862f98660eb3ec544
iretq <32851089+48cf@users.noreply.github.com>
2024-05-08 05:27:33 +0200

    misc/create_image: Build a hybrid ISO

--- misc/create-image.sh
+++ misc/create-image.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -ex
 
@@ -10,19 +10,25 @@ rm -rf sysroot
 # Make an initramfs tarball from the sysroot.
 (cd sysroot && tar cf ../initramfs.tar *)
 
+limine_dir="host-pkgs/limine/usr/local/"
+
 # Prepare the ISO root.
 rm -rf iso_root
 mkdir -p iso_root
+mkdir -p iso_root/EFI/BOOT
 cp pkgs/kernel/usr/bin/kernel iso_root/
 cp initramfs.tar iso_root/
 cp misc/limine.cfg iso_root/
-cp host-pkgs/limine/usr/local/share/limine/limine-bios.sys iso_root/
-cp host-pkgs/limine/usr/local/share/limine/limine-bios-cd.bin iso_root/
+cp $limine_dir/share/limine/limine-bios.sys iso_root/
+cp $limine_dir/share/limine/limine-bios-cd.bin iso_root/
+cp $limine_dir/share/limine/limine-uefi-cd.bin iso_root/
+cp $limine_dir/share/limine/BOOTX64.EFI iso_root/EFI/BOOT/
 
 # Create the ISO.
 xorriso -as mkisofs -b limine-bios-cd.bin \
-  -no-emul-boot -boot-load-size 4 -boot-info-table \
-  --protective-msdos-label iso_root -o zigux.iso
+    -no-emul-boot -boot-load-size 4 -boot-info-table \
+    --efi-boot limine-uefi-cd.bin -efi-boot-part --efi-boot-image \
+    --protective-msdos-label iso_root -o zigux.iso
 
 # Install Limine.
-host-pkgs/limine/usr/local/bin/limine bios-install zigux.iso
+$limine_dir/bin/limine bios-install zigux.iso
