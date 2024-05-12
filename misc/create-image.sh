2172fdcf739d1e21d601c525bc4409a709848580
iretq <32851089+48cf@users.noreply.github.com>
2024-05-12 14:11:02 +0200

    misc: Compress the initramfs when creating an image

--- misc/create-image.sh
+++ misc/create-image.sh
@@ -7,9 +7,6 @@ rm -rf sysroot
 ./jinx sysroot
 ./jinx host-build limine
 
-# Make an initramfs tarball from the sysroot.
-(cd sysroot && tar cf ../initramfs.tar *)
-
 limine_dir="host-pkgs/limine/usr/local/"
 
 # Prepare the ISO root.
@@ -17,12 +14,28 @@ rm -rf iso_root
 mkdir -p iso_root
 mkdir -p iso_root/EFI/BOOT
 cp pkgs/kernel/usr/bin/kernel iso_root/
-cp initramfs.tar iso_root/
-cp misc/limine.cfg iso_root/
 cp $limine_dir/share/limine/limine-bios.sys iso_root/
 cp $limine_dir/share/limine/limine-bios-cd.bin iso_root/
 cp $limine_dir/share/limine/limine-uefi-cd.bin iso_root/
 cp $limine_dir/share/limine/BOOTX64.EFI iso_root/EFI/BOOT/
+
+# Make an initramfs tarball from the sysroot.
+if type pigz >/dev/null 2>&1; then
+    tar -I pigz -C sysroot -cf iso_root/initramfs.tar.gz .
+else
+    tar -C sysroot -czf iso_root/initramfs.tar.gz .
+fi
+
+# Write the limine config
+cat <<EOF >iso_root/limine.cfg
+TIMEOUT=0
+SERIAL=yes
+
+:kernel
+    PROTOCOL=limine
+    KERNEL_PATH=boot:///kernel
+    MODULE_PATH=\$boot:///initramfs.tar.gz
+EOF
 
 # Create the ISO.
 xorriso -as mkisofs -b limine-bios-cd.bin \
